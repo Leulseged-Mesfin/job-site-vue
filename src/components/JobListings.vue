2c3623507d54a4925add543af87f5a84e7e803cf
Leulseged-Mesfin <leulsegedmesfin16@gmail.com>
2024-12-30 23:29:48 +0300

    added additional features like making the app a SPA.

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,8 @@
 <script setup>
 import jobData from "@/jobs.json";
-import { ref, defineProps } from "vue";
+import { ref } from "vue";
 import JobListing from "./JobListing.vue";
+import { RouterLink } from 'vue-router';
 
 const jobs = ref(jobData);
 
@@ -22,7 +23,7 @@ defineProps({
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || length(jobs))"
+          v-for="job in jobs.slice(0, limit || jobs.length)"
           :key="job.id"
           :job="job"
         />
@@ -30,10 +31,9 @@ defineProps({
     </div>
   </section>
   <section v-if="showbutton" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
+    <RouterLink
+      to="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
-    >
+      >View All Jobs</RouterLink>
   </section>
 </template>
