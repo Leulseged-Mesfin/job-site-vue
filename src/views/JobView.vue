bc9b53733d1df0d8cd0539cc1b94e715fa4946af
Leulseged-Mesfin <leulsegedmesfin16@gmail.com>
2025-01-01 20:49:21 +0300

    added editing and deleting jobs functionality.

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -2,9 +2,13 @@
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import BackButtonView from '@/components/BackButtonView.vue';
 import { reactive, onMounted } from 'vue';
-import { RouterLink, useRoute } from 'vue-router';
+import { useToast } from "vue-toastification";
+import { RouterLink, useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 
+
+const router = useRouter()
+const toast = useToast()
 const route = useRoute()
 const jobId = route.params.id
 
@@ -12,6 +16,20 @@ const state = reactive({
   job: {},
   isLoading: true,
 })
+
+const deleteJob = async () => {
+  try {
+    const confirmDelete = window.confirm('Are you sure you want to delete this job?');
+    if (confirmDelete) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job deleted successfully');
+      router.push('/jobs');
+    }
+  } catch (error) {
+    console.error('error deleting job', error);
+    toast.error('Job was not deleted.');
+  }
+}
 
 
 onMounted(async () => {
@@ -97,7 +115,7 @@ onMounted(async () => {
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</RouterLink
               >
-              <button
+              <button @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
